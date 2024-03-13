90dbb1a7803ab55536e55c23fac2b444b891cd22
Vander Moleker <noierdev@proton.me>
2024-03-13 06:40:13 -0700

    first init

--- controllers/audios.controller.js
+++ controllers/audios.controller.js
@@ -1,9 +1,19 @@
 const fs=require('fs')
 const path=require('path');
 
+function createDirectoryIfNotExists(path) {
+    if (!fs.existsSync(path)) {
+      fs.mkdirSync(path);
+      console.log('Directory created successfully.');
+    } else {
+      console.log('Directory already exists.');
+    }
+  }
+
 exports.transcribe=(req,res)=>{
     if(!req.files) return res.json({status:"error",error:"NO_FILE"})
     const audioFile=req.files.file;
-    console.log(audioFile)
+    console.log(audioFile);
+    createDirectoryIfNotExists(path.resolve(__dirname,"../temp"));
     return res.json({status:"success"})
 }
