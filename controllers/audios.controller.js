7916f179fdf11be80ad2ac02637d1a2ae9f717f6
Vander Moleker <noierdev@proton.me>
2024-03-13 21:49:50 -0700

    stream pass

--- controllers/audios.controller.js
+++ controllers/audios.controller.js
@@ -12,28 +12,29 @@ function createDirectoryIfNotExists(path) {
     }
   }
 
-exports.transcribe=(req,res)=>{
+exports.transcribe=async (req,res)=>{
     if(!req.files) return res.json({status:"error",error:"NO_FILE"})
     const audioFile=req.files.file;
     console.log(audioFile);
-    createDirectoryIfNotExists(path.resolve(__dirname,"../temp"));
-    audioFile.mv(path.resolve(__dirname,"../temp",audioFile.name),async ()=>{
-        const stream=await Readable.from(audioFile.data)
-        const myForm=new FormData();
-        myForm.append('file',stream,{filename:audioFile.name});
-        myForm.append('model',"whisper-1");
-        axios.post('https://api.openai.com/v1/audio/transcriptions',myForm,{
-           headers:{
-            "Authorization":`Bearer ${process.env.OPENAI_API_KEY}`,
-            ...myForm.getHeaders()
-           } 
-        })
-        .then(response=>{
-            return res.json({status:"success",data:response.data})
-        }).catch(e=>{
-            return res.json({status:"error",error:e})
-        })
+    // createDirectoryIfNotExists(path.resolve(__dirname,"../temp"));
+    const stream= Readable.from(audioFile.data)
+    const myForm=new FormData();
+    myForm.append('file',stream,{filename:audioFile.name});
+    myForm.append('model',"whisper-1");
+    axios.post('https://api.openai.com/v1/audio/transcriptions',myForm,{
+        headers:{
+        "Authorization":`Bearer ${process.env.OPENAI_API_KEY}`,
+        ...myForm.getHeaders()
+        } 
+    })
+    .then(response=>{
+        return res.json({status:"success",data:response.data})
+    }).catch(e=>{
+        return res.json({status:"error",error:e})
+    })
+    // audioFile.mv(path.resolve(__dirname,"../temp",audioFile.name),async ()=>{
         
-    });
+        
+    // });
     
 }
